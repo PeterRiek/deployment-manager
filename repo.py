a7ca149ef4609c5635bdc911224ee410b85743e4
Peter Riek <peter.riek@gmx.de>
2025-12-30 21:03:23 +0100

    debugging

--- repo.py
+++ repo.py
@@ -3,19 +3,22 @@ import os
 
 def check_repo(directory: str, repo_url: str) -> bool:
     try:
-        subprocess.run(
+        res = subprocess.run(
             ["git", "rev-parse", "--is-inside-work-tree"],
             cwd=directory, check=True,
             stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
+        ).stdout.strip()
+        print("[check_repo]", res)
         url = subprocess.run(
             ["git", "config", "--get", "remote.origin.url"],
             cwd=directory, check=True,
             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
             text=True
         ).stdout.strip()
+        print("[check_repo]", url)
         return url == repo_url
     except subprocess.CalledProcessError:
+        print("[check_repo] CalledProcessError")
         return False
 
 def clone_repo(directory: str, repo_url: str, branch: str):
